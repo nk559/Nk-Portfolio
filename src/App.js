ebdddeab7bd6e76ce374f01235d32f449d053e83
nk559 <nareshkumaranandan77@gmail.com>
2024-07-31 10:58:15 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import Hooks from './Componant/Hooks';
+//import './App.css';
+import Class from './Componant/Class';
+ import Function from './Componant/Function'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Components</h1>
+       <Class/> 
+       <Function/> 
+    {/* <Hooks/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componant/Class.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Class extends React.Component{
+    render(){
+        return(
+            <div>It is my first class component</div>
+        )
+    }
+}
+export default Class;
